5fc162c5885b9396ee52df5b75ffbf4f40c402ce
Pedro Henrique De Medeiros Gomes <113909282+PedrohenriDMG@users.noreply.github.com>
2023-10-05 16:45:22 -0300

    Add project files.

---
+++ TreinoPOO/Entities/Enum/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TreinoPOO.Entities.Enum
+{   
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }   
+}
---
+++ TreinoPOO/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using TreinoPOO.Entities;
+using TreinoPOO.Entities.Enum;
+
+namespace TreinoPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client Data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) 
+            {
+                Console.WriteLine("Enter #" + i + " item data: ");
+                Console.Write("Product name: ");
+                string Pname = Console.ReadLine();
+                Console.Write("Product price: ");
+                double Pprice = double.Parse(Console.ReadLine());
+
+                Product product = new Product(Pname, Pprice);
+
+                Console.Write("Quantity: ");
+                int Pquantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderitem = new OrderItem(Pquantity, Pprice, product);  
+
+                order.AddItem(orderitem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+        }
+    }
+}
